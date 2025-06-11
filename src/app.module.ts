a66db415114bb36398dd523aacd5c033565fe130
murilodamarioo <murilo.gcdamario@gmail.com>
2025-06-10 22:33:19 -0300

    :sparkles: feat: Uses Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './infra/database/prisma/prisma.service';
 import { CreateAdminAccountController } from './infra/http/controllers/create-admin-account.controller';
+import { envSchema } from './env';
 
 @Module({
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true
+  })],
   controllers: [CreateAdminAccountController],
   providers: [PrismaService],
 })
